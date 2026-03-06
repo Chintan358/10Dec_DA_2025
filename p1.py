d0ecef107594aaeae8036638b337c39ce7a2a990
chintan <chintan@gmail.com>
2026-03-06 12:31:35 +0530

    all

--- p1.py
+++ p1.py
@@ -1,20 +1,26 @@
-# *****
-# *****
-# *****
-# *****
-# *****
+# # *****
+# # *****
+# # *****
+# # *****
+# # *****
 
-lines = 5
-for i in range(lines):
-    for k in range(lines-i):
-        print(" ",end="")
-    for j in range(i+1):
-        print("*",end="")
-    print()
-lines = 4
-for i in range(lines):
-    for k in range(i+2):
-        print(" ",end="")
-    for j in range(lines-i):
-        print("*",end="")
-    print()
+# lines = 5
+# for i in range(lines):
+#     for k in range(lines-i):
+#         print(" ",end="")
+#     for j in range(i+1):
+#         print("*",end="")
+#     print()
+# lines = 4
+# for i in range(lines):
+#     for k in range(i+2):
+#         print(" ",end="")
+#     for j in range(lines-i):
+#         print("*",end="")
+#     print()
+
+import re
+
+a = "movie is not fdfd poor "
+k = re.sub(r"not\s+\w+\s+poor", "good", a)
+print(k)
