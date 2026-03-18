2b9477705c7b8a2660ed8b7a430b054ad16530a1
chintan <chintan@gmail.com>
2026-03-18 12:30:34 +0530

    all

--- 012_Numpy/ArraySpilte.py
+++ 012_Numpy/ArraySpilte.py
@@ -5,6 +5,6 @@ import numpy as np
 # print(k[1])
 
 
-a = np.array([[10,20,30,78,70],[40,50,60,89,75]])
+a = np.array([[10,20,30,78,70],[40,50,60,9,75]])
 k = np.array_split(a,2,axis=1)
 print(k)
---
+++ 012_Numpy/arithmatic.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# a = np.array([10,20,30])
+# b = np.array([4,5,6])
+
+# k  =np.add(a,b)
+# k = np.subtract(a,b)
+# k = np.multiply(a,b)
+# k = np.divide(a,b)
+# k = np.power(a,b)
+# print(k)
+
+a = np.array([[1,2],
+              [3,4]]
+                     )
+b = np.array([[5,6],
+              [7,9]])
+
+# k  =np.sum((a,b),axis=1)
+# print(k)
